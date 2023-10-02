0f648fe8179c7b9134406e8708134060f945953e
Naruto-242 <144617151+Naruto-242@users.noreply.github.com>
2023-10-02 13:43:11 +0530

    beryllium: Import missing libplatformconfig.so -1

--- beryllium-vendor.mk
+++ beryllium-vendor.mk
@@ -331,5 +331,6 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.fm@1.0 \
+    libplatformconfig \
     android.hardware.gnss@2.0-service-qti \
     vendor.qti.gnss@3.0-service
